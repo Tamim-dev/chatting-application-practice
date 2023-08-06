87733ba201c24ec1805ecf85cea38abd460b0e89
Tanvir Mahmud Tamim <tanvirmahmudtamim59@gmail.com>
2023-08-06 17:34:39 +0600

    login ragitration done

--- src/components/pages/login/Login.jsx
+++ src/components/pages/login/Login.jsx
@@ -5,6 +5,8 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import imgp from "../../../assets/react.svg";
+import Alert from "@mui/material/Alert";
 
 let initialValue = {
     email: "",
@@ -14,21 +16,38 @@ let initialValue = {
 
 const Login = () => {
     const auth = getAuth();
-    let navigate = useNavigate()
+    let navigate = useNavigate();
     let [values, setValues] = useState(initialValue);
 
     let handelChange = (e) => {
         setValues({
             ...values,
             [e.target.name]: e.target.value,
+            error:""
         });
     };
 
     let handelClick = () => {
         let { email, password } = values;
+
+        if (!email) {
+            setValues({
+                ...values,
+                error: "Enteryouremail",
+            });
+            return;
+        }
+        if (!password) {
+            setValues({
+                ...values,
+                error: "Enteryourpassword",
+            });
+            return;
+        }
+
         signInWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
-                navigate("/rotlayout")
+                navigate("/rotlayout");
             })
             .catch((error) => {
                 const errorCode = error.code;
@@ -39,6 +58,11 @@ const Login = () => {
 
     return (
         <div className="inputbox">
+            <div>
+                <img style={{ marginTop: "100px" }} src={imgp} />
+                <h2 style={{ margin: "42px 0 12px 0" }}>Login</h2>
+                <p>Login and you can enjoy it</p>
+            </div>
             <div className="textfield">
                 <TextField
                     onChange={handelChange}
@@ -47,6 +71,11 @@ const Login = () => {
                     label="Email"
                     variant="outlined"
                 />
+                {values.error.includes("Enteryouremail") && (
+                    <Alert className="alert" severity="error">
+                        Please Enter Your Email
+                    </Alert>
+                )}
             </div>
             <div className="textfield">
                 <TextField
@@ -56,10 +85,15 @@ const Login = () => {
                     label="Password"
                     variant="outlined"
                 />
+                {values.error.includes("Enteryourpassword") && (
+                    <Alert className="alert" severity="error">
+                        Please Enter Your Password
+                    </Alert>
+                )}
             </div>
             <div>
                 <Button onClick={handelClick} variant="contained">
-                    Ragistration
+                    Login
                 </Button>
             </div>
         </div>
--- src/components/pages/ragistration/Ragistration.jsx
+++ src/components/pages/ragistration/Ragistration.jsx
@@ -4,7 +4,10 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import imgp from "../../../assets/react.svg";
+import Alert from "@mui/material/Alert";
+import Stack from "@mui/material/Stack";
 
 let initialValue = {
     email: "",
@@ -15,21 +18,45 @@ let initialValue = {
 
 const Ragistration = () => {
     const auth = getAuth();
-    let navigate = useNavigate()
+    let navigate = useNavigate();
     let [values, setValues] = useState(initialValue);
 
     let handelChange = (e) => {
         setValues({
             ...values,
             [e.target.name]: e.target.value,
+            error: "",
         });
     };
 
     let handelClick = () => {
-        let {email,password} = values
+        let { email, fullName, password } = values;
+
+        if (!email) {
+            setValues({
+                ...values,
+                error: "Enteryouremail",
+            });
+            return;
+        }
+        if (!fullName) {
+            setValues({
+                ...values,
+                error: "Enteryourfullname",
+            });
+            return;
+        }
+        if (!password) {
+            setValues({
+                ...values,
+                error: "Enteryourpassword",
+            });
+            return;
+        }
+
         createUserWithEmailAndPassword(auth, email, password)
             .then(() => {
-                navigate("/login")
+                navigate("/login");
             })
             .catch((error) => {
                 console.log(error.message);
@@ -38,6 +65,13 @@ const Ragistration = () => {
 
     return (
         <div className="inputbox">
+            <div>
+                <img style={{ marginTop: "100px" }} src={imgp} />
+                <h2 style={{ margin: "42px 0 12px 0" }}>
+                    Get started with easily register
+                </h2>
+                <p>Free register and you can enjoy it</p>
+            </div>
             <div className="textfield">
                 <TextField
                     onChange={handelChange}
@@ -46,6 +80,11 @@ const Ragistration = () => {
                     label="Email"
                     variant="outlined"
                 />
+                {values.error.includes("Enteryouremail") && (
+                    <Alert className="alert" severity="error">
+                        Please Enter Your Email
+                    </Alert>
+                )}
             </div>
             <div className="textfield">
                 <TextField
@@ -55,6 +94,11 @@ const Ragistration = () => {
                     label="Full Name"
                     variant="outlined"
                 />
+                {values.error.includes("Enteryourfullname") && (
+                    <Alert className="alert" severity="error">
+                        Please Enter Your Full Name
+                    </Alert>
+                )}
             </div>
             <div className="textfield">
                 <TextField
@@ -64,12 +108,20 @@ const Ragistration = () => {
                     label="Password"
                     variant="outlined"
                 />
+                {values.error.includes("Enteryourpassword") && (
+                    <Alert className="alert" severity="error">
+                        Please Enter Your Password
+                    </Alert>
+                )}
             </div>
             <div>
                 <Button onClick={handelClick} variant="contained">
                     Ragistration
                 </Button>
             </div>
+            <Alert className="alert" severity="info">
+                         Allready have an account  <Link to="/login" style={{color:"#DA8F21", cursor:"pointer"}}>Sing In</Link>
+                    </Alert>
         </div>
     );
 };
