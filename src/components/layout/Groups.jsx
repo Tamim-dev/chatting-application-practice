264e4ddc261f50fdc103311b8ea09f7ff9cd9d9c
Tanvir Mahmud Tamim <tanvirmahmudtamim59@gmail.com>
2023-08-12 02:45:02 +0600

    push

--- src/components/layout/Groups.jsx
+++ src/components/layout/Groups.jsx
@@ -1,27 +1,90 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "@mui/material/Button";
 import profile from "../../assets/profile.png";
+import {
+    getDatabase,
+    ref,
+    onValue,
+    set,
+    push,
+    remove,
+} from "firebase/database";
+import { useSelector } from "react-redux";
 
 const Groups = () => {
+    const db = getDatabase();
+    let userList = useSelector((state) => state.loggedUser.loginUser);
+    let [myGroup, setMyGroup] = useState([]);
+    let [GroupJoinReq, setGroupJoinReq] = useState([]);
+
+
+    useEffect(() => {
+        onValue(ref(db, "groups/"), (snapshot) => {
+            let arr = [];
+            snapshot.forEach((item) => {
+                if (userList.uid != item.val().adminId) {
+                    arr.push({ ...item.val(), id: item.key });
+                }
+            });
+            setMyGroup(arr);
+        });
+
+        onValue(ref(db, "groupjoinreq/"), (snapshot) => {
+            let arr = [];
+            snapshot.forEach((item) => {
+                // if(item.val().userId == userList.uid){
+
+                    arr.push(item.val().groupId);
+                // }
+            });
+            setGroupJoinReq(arr);
+        });
+    }, []);
+
+    let handelJoinReq = (item) => {
+        set(push(ref(db, "groupjoinreq/")), {
+            userId: userList.uid,
+            groupId: item.id,
+        });
+       
+    };
+
     return (
         <div className="box">
-                            <div className="tilte">
-                                <h3>Group</h3>
-                            </div>
-                            <div className="user">
-                                <img className="profileimg" src={profile} />
-                                <div className="profiletitle">
-                                    <h4>Jenny Wilson</h4>
-                                    <p>Love You.....</p>
-                                </div>
-                                <div className="profilebtn">
-                                    <Button variant="contained" size="small">
-                                        Join
-                                    </Button>
-                                </div>
-                            </div>
-                        </div>
-      );
+            <div className="tilte">
+                <h3>Group</h3>
+            </div>
+            {myGroup.map((item) => (
+                <div className="user">
+                    <img className="profileimg" src={profile} />
+                    <div className="profiletitle">
+                        <p style={{ fontSize: "10px" }}>
+                            Admin: {item.adminName}
+                        </p>
+                        <h4>{item.groupName}</h4>
+                        <p style={{ fontSize: "12px" }}>{item.groupTagline}</p>
+                    </div>
+                    <div className="profilebtn">
+                        {GroupJoinReq.indexOf(item.id) != -1 ?
+                        <Button
+                            variant="contained"
+                            size="small"
+                        >
+                            Requset
+                        </Button>
+                        :
+                        <Button
+                            onClick={() => handelJoinReq(item)}
+                            variant="contained"
+                            size="small"
+                        >
+                            Join
+                        </Button>}
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default Groups;
--- src/components/layout/Mygroups.jsx
+++ src/components/layout/Mygroups.jsx
@@ -1,69 +1,103 @@
-import React,{useState} from "react";
+import React, { useState ,useEffect} from "react";
 import Button from "@mui/material/Button";
 import profile from "../../assets/profile.png";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
-import TextField from '@mui/material/TextField';
-
+import TextField from "@mui/material/TextField";
+import {
+    getDatabase,
+    ref,
+    onValue,
+    set,
+    push,
+    remove,
+} from "firebase/database";
+import { useSelector } from "react-redux";
 
 const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
     width: 350,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
+    bgcolor: "background.paper",
+    border: "2px solid #000",
     boxShadow: 24,
     p: 4,
-    textAlign: 'center',
-  };
-  let initialValue = {
+    textAlign: "center",
+};
+let initialValue = {
     name: "",
     tagline: "",
 };
 
 const Mygroups = () => {
+    const db = getDatabase();
     const [open, setOpen] = useState(false);
     const [values, setValues] = useState(initialValue);
     const handleClose = () => setOpen(false);
+    const handleOpen = () => setOpen(true);
+    let userList = useSelector((state) => state.loggedUser.loginUser);
+    let [myGroup,setMyGroup] = useState([])
 
-    let handelchange =(e)=>{
+    useEffect(() => {
+        onValue(ref(db, "groups/"), (snapshot) => {
+            let arr = [];
+            snapshot.forEach((item) => {
+                if (userList.uid == item.val().adminId) {
+                    arr.push({ ...item.val(), id: item.key });
+                }
+            });
+            setMyGroup(arr);
+        });
+    }, []);
+
+    let handelchange = (e) => {
         setValues({
             ...values,
-            [e.target.name]: e.target.value
-        })
-    }
+            [e.target.name]: e.target.value,
+        });
+    };
 
     let handelCreactGroup = () => {
-        setOpen(true);
+        set(push(ref(db, "groups/")), {
+            adminId: userList.uid,
+            adminName: userList.displayName,
+            groupName: values.name,
+            groupTagline: values.tagline,
+        }).then(() => {
+            setOpen(false);
+        });
     };
 
     return (
         <div className="box">
             <div className="tilte">
                 <h3>My Groups</h3>
-                <Button
-                    onClick={handelCreactGroup}
-                    variant="contained"
-                    size="small"
-                >
+                <Button onClick={handleOpen} variant="contained" size="small">
                     Creact Group
                 </Button>
             </div>
-            <div className="user">
+            {myGroup.map(item=>(
+                <div className="user">
                 <img className="profileimg" src={profile} />
                 <div className="profiletitle">
-                    <h4>Jenny Wilson</h4>
-                    <p>Love You.....</p>
+                    <p style={{fontSize:"10px"}}>Admin: {item.adminName}</p>
+                    <h4>{item.groupName}</h4>
+                    <p style={{fontSize:"12px"}}>{item.groupTagline}</p>
                 </div>
-                <div className="profilebtn">
-                    <Button variant="contained" size="small">
-                        Join
+                <div className="frireqebtn">
+                    <Button variant="contained" color="secondary" size="small">
+                        Requset
+                    </Button>
+                    <Button variant="contained" color="success" size="small">
+                        Members
                     </Button>
                 </div>
             </div>
+            ))}
+            
 
             <Modal
                 open={open}
@@ -77,18 +111,30 @@ const Mygroups = () => {
                         variant="h6"
                         component="h2"
                     >
-                    Creact Group
+                        Creact Group
                     </Typography>
                     <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                    <TextField onChange={handelchange} name="name" id="outlined-basic" label="Outlined" variant="outlined" />
-                    <TextField onChange={handelchange} name="tagline" id="outlined-basic" label="Outlined" variant="outlined" />
-                    <Button
-                    onClick={handelCreactGroup}
-                    variant="contained"
-                    size="small"
-                >
-                    Creact
-                </Button>
+                        <TextField
+                            onChange={handelchange}
+                            name="name"
+                            id="outlined-basic"
+                            label="Outlined"
+                            variant="outlined"
+                        />
+                        <TextField
+                            onChange={handelchange}
+                            name="tagline"
+                            id="outlined-basic"
+                            label="Outlined"
+                            variant="outlined"
+                        />
+                        <Button
+                            onClick={handelCreactGroup}
+                            variant="contained"
+                            size="small"
+                        >
+                            Creact
+                        </Button>
                     </Typography>
                 </Box>
             </Modal>
